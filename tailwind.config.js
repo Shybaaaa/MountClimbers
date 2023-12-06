f2a3b767229e707640802c5123e5a0d1ce44ff43
Shyba <diablotin180@gmail.com>
2023-12-06 09:44:56 +0100

    push

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "./public/**/*.{html,js,php}",
+      "./private/**/*.{html,js,php}",
+      "./index.php"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
